8a6f8c54b47596189df35d7829b0d9c36d4d961f
Josh <82219335+i-suck-at-most-stuff@users.noreply.github.com>
2025-06-24 03:26:02 +0000

    Light/Dark Mode Update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,43 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.getElementById('navbar');
-  const hero = document.getElementById('hero');
+const toggleButton = document.getElementById('mode-toggle');
+  const modeIcon = document.getElementById('mode-icon');
+  const body = document.body;
   const logoSmall = document.getElementById('logo-small');
-  const scrollTrigger = hero.offsetHeight * 0.3;
 
-  if (window.scrollY > scrollTrigger) {
-    navbar.classList.add('shrink');
-  } else {
-    navbar.classList.remove('shrink');
-  }
-});
+  
+  let darkMode = false;
 
+  toggleButton.addEventListener('click', () => {
+    darkMode = !darkMode;
+
+    if (darkMode) {
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
+      logoSmall.src = '/images/logos/coders_white_cropped.png';
+      modeIcon.classList.remove('fa-moon');
+      modeIcon.classList.add('fa-sun');
+    } else {
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
+      logoSmall.src = '/images/logos/logo-green-ver.png';
+      modeIcon.classList.remove('fa-sun');
+      modeIcon.classList.add('fa-moon');
+    }
+  });
+
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    body.classList.add('light-mode');
+  });
+
+  
+  window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    const hero = document.getElementById('hero');
+    const scrollTrigger = hero.offsetHeight * 0.3;
+
+    if (window.scrollY > scrollTrigger) {
+      navbar.classList.add('shrink');
+    } else {
+      navbar.classList.remove('shrink');
+    }
+  });
